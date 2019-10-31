dbf3ecca10641e2f8430550858450945f326424b
Marcela Rosales <marcelarosalesj@gmail.com>
2019-10-31 16:42:39 -0600

    vision-sense: add image capture phase
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- vision-sense/README.md
+++ vision-sense/README.md
@@ -1,3 +1,15 @@
 # vision-sense
 
 Get an image from a camera, store it in `../image-storage` and send a request to image-processing/bw
+
+```
+# Build image
+docker build --build-arg http_proxy=$PROXY --build-arg https_proxy=$PROXY -t r4-vs .
+
+# For X11 forwarding
+DISPLAY=:0.0 ; export DISPLAY
+xhost +local:docker
+
+# Create container
+docker run --device=/dev/video0:/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -p 5000:5000 -p 8888:8888 --name r4-cont r4-vs:latest
+```
--- vision-sense/app.py
+++ vision-sense/app.py
@@ -1,30 +1,48 @@
 import requests
+import cv2
 
-#
-# TO-DO: get an image, store it in ../image-storage then call to image-processing/bw
-#
+def list_images():
+    """
+    /image-processing/images
+    """
+    resp = requests.get('http://r4-ip:8080/image-processing/images')
+    if resp.status_code != 200:
+        # This means something went wrong.
+        raise Exception('GET /image-processing/images/ {}'.format(resp.status_code))
+    for todo_item in resp.json()['images']:
+        print('{} {} {}'.format(todo_item['id'], todo_item['path'], todo_item['path-bw']))
 
 
-# /image-processing/images
-resp = requests.get('http://localhost:5000/image-processing/images')
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /image-processing/images/ {}'.format(resp.status_code))
-for todo_item in resp.json()['images']:
-    print('{} {} {}'.format(todo_item['id'], todo_item['path'], todo_item['path-bw']))
+print("starting vision-sense app")
 
+list_images()
 
-# /image-processing/bw
-image = {"path": "../image-storage/img04.jpg"}
-resp = requests.post('http://localhost:5000/image-processing/bw', json=image)
-if resp.status_code != 201:
-    raise ApiError('POST /tasks/ {}'.format(resp.status_code))
-print('Convert image. ID: {}'.format(resp.json()["image"]['id']))
+cap = cv2.VideoCapture(0)
+cont = 0
 
-# /image-processing/images
-resp = requests.get('http://localhost:5000/image-processing/images')
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET /image-processing/images/ {}'.format(resp.status_code))
-for todo_item in resp.json()['images']:
-    print('{} {} {}'.format(todo_item['id'], todo_item['path'], todo_item['path-bw']))
+while(True):
+    ret, img = cap.read()
+
+    cv2.imshow('Camera', img)
+
+    k = cv2.waitKey(1)
+    if k == 27: # ESC
+        print('ESC')
+        break
+    elif k == 115: #s
+        print('shoot-store-process')
+        file_name = '/image-storage/img{}.jpg'.format(cont)
+        cv2.imwrite(file_name, img)
+        cont = cont + 1
+        image = {"path": file_name}
+        resp = requests.post('http://r4-ip:8080/image-processing/bw', json=image)
+        if resp.status_code != 201:
+            raise Exception('POST /tasks/ {}'.format(resp.status_code))
+        print('Convert image. ID: {}'.format(resp.json()["image"]['id']))
+
+cap.release()
+cv2.destroyAllWindows()
+
+list_images()
+
+print("finishing vision-sense app")
