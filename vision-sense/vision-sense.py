8fe739896639c4469dede5f59a41da45170f298f
Marcela Rosales <marcelarosalesj@gmail.com>
2019-11-04 16:12:01 -0600

    vision-sense [dev]: capture image and store it
    Signed-off-by: Marcela Rosales <marcelarosalesj@gmail.com>

--- vision-sense/vision-sense.py
+++ vision-sense/vision-sense.py
@@ -1,5 +1,4 @@
 import zmq
-import time
 import cv2
 
 context = zmq.Context()
@@ -8,11 +7,24 @@ address = "tcp://0.0.0.0:5557"
 socket.bind(address)
 print("Sending to {}...".format(address))
 
+cap = cv2.VideoCapture(0)
+
 cont = 0
 while True:
-    file_name = '/image-storage/img{}.jpg'.format(cont)
-    cont = cont + 1
-    image_json = {"path": file_name}
-    socket.send_json(image_json)
-    print("Sent message")
-    time.sleep(1)
+    ret, img = cap.read()
+    cv2.imshow('Camera', img)
+    k = cv2.waitKey(1)
+    if k == 27: # ESC
+        print('ESC')
+        break
+    elif k == 115: # s
+        print('shoot-store-process')
+        file_name = '/image-storage/img{}.jpg'.format(cont)
+        cv2.imwrite(file_name, img)
+        cont = cont + 1
+        image_json = {"path": file_name}
+        socket.send_json(image_json)
+        print("Sent message")
+
+cap.release()
+cv2.destroyAllWindows()
